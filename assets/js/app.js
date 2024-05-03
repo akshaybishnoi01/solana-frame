9b033635143bbe14b1dcf83abfd01012faefc298
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-05-03 10:19:10 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+
+
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
