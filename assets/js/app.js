06f8e28bd0d65ecc671cc3164795da68f1816758
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-05-04 12:38:32 +0530

    page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 
 
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
-  }
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
 
 
 
@@ -76,6 +76,46 @@ function openNav() {
 
 
 
-  const y = new Date();
+
+
+
+const y = new Date();
 let Year = y.getFullYear();
 document.getElementById("year2").innerHTML = Year
+
+$('.responsive').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: true,
+  prevArrow: ".prev",
+  nextArrow: ".next",
+
+
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade: true,
+      }
+    },
+  ]
+});
+
+AOS.init({
+  duration: 1500,
+})
