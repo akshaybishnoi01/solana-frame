af6ae698005acba120c8bad7c70d76aa1d4946db
akshaybishnoi01 <akshaybishnoi0008@gmail.com>
2024-05-04 10:25:54 +0530

    meta tag

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,35 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu").classList.toggle("cross")
   }
+  $('.responsive').slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: true,
+  prevArrow: ".prev",
+  nextArrow: ".next",
+ 
+
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        fade: true,
+      }
+    },
+  ]
+});
